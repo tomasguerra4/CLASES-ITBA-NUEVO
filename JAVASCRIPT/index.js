79972eecdd06af4749355b0a17135840ca78b990
tomasguerra4 <tomasguerra308@outlook.com>
2022-05-25 21:57:52 -0300

    JS PRIMERA CLASE

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,17 @@
+alert('funcionando');
+
+//Variables
+
+Var nombre-de-la-variable = valor-de-la-variable; //Hay varios tipos de variables y cada una cambia su forma de escribirse. String, number
+
+
+// Alerta
+Alert (nombre-de-la-varibale);
+
+//Constantes
+
+const numero2= 10
+
+// Interaccion con la consola
+
+console.log(numero2);
---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,19 @@
+alert('hola mundo!!!');
+
+var nombre = 'tomas guerra'
+var altura = '180'
+var concatenacion= nombre + " " + altura
+
+
+document.write (concatenacion);
+
+var datos = document.getElementById("datos"):
+
+datos.innerHTML =
+<h1>Soy la caja de datos</h1>
+<h2> Mi nombre es ${nombre}</h2>
+<h3> Mi altura es ${altura}</h3>
+:
+
+
+
